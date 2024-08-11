26743d103ecf541caaddc18c3ffdbed02dbae49e
Christianbello1 <lulabchannel2@gmail.com>
2024-08-12 00:28:11 +0200

    datapicker2

--- frontend/src/pages/CustomDatePicker.jsx
+++ frontend/src/pages/CustomDatePicker.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import './CustomDatePicker.css'; // We'll create this file for custom styles
+import './CustomDatePicker.css';
 
-const CustomDatePicker = ({ selected, onChange, ...props }) => {
+const CustomDatePicker = ({ selected, onChange, placeholderText, ...props }) => {
+  const years = Array.from({ length: 100 }, (_, i) => new Date().getFullYear() - i);
+  const months = [
+    "Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno",
+    "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"
+  ];
+
   return (
     <DatePicker
       selected={selected}
@@ -11,6 +17,52 @@ const CustomDatePicker = ({ selected, onChange, ...props }) => {
       dateFormat="dd/MM/yyyy"
       className="custom-datepicker"
       calendarClassName="custom-calendar"
+      placeholderText={placeholderText}
+      showYearDropdown
+      scrollableYearDropdown
+      yearDropdownItemNumber={100}
+      showMonthDropdown
+      dropdownMode="select"
+      renderCustomHeader={({
+        date,
+        changeYear,
+        changeMonth,
+        decreaseMonth,
+        increaseMonth,
+        prevMonthButtonDisabled,
+        nextMonthButtonDisabled,
+      }) => (
+        <div className="custom-header">
+          <button onClick={decreaseMonth} disabled={prevMonthButtonDisabled}>
+            {"<"}
+          </button>
+          <select
+            value={date.getFullYear()}
+            onChange={({ target: { value } }) => changeYear(value)}
+          >
+            {years.map((option) => (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+          <select
+            value={months[date.getMonth()]}
+            onChange={({ target: { value } }) =>
+              changeMonth(months.indexOf(value))
+            }
+          >
+            {months.map((option) => (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+          <button onClick={increaseMonth} disabled={nextMonthButtonDisabled}>
+            {">"}
+          </button>
+        </div>
+      )}
       {...props}
     />
   );
