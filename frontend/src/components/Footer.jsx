038332109d6767cc394b20fe3ba37d1bb761ba91
Christianbello1 <lulabchannel2@gmail.com>
2024-08-28 11:03:53 +0200

    footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="scrolling-text">
-        In caso di problemi come scritte sbagliate, info non corrette, ecc.  manda email a: lulabchannel2@gmail.com
+        In caso di problemi come scritte sbagliate, info non corrette, ecc. o anche segnalazioni commenti manda email a: lulabchannel2@gmail.com
       </div>
     </footer>
   );
